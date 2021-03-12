7072856b21851a596f15a2a41e5a7cc9ecc13540
Ruud Verheijden <ruud.verheijden@gmail.com>
2021-03-12 19:29:41 +0100

    Add photos from array and spread better across the screen

--- js/main.js
+++ js/main.js
@@ -1,27 +1,53 @@
-let photos = [...document.querySelectorAll('.photo')]; // Convert Nodelist to Array
+const photos = [
+    "https://picsum.photos/id/1/800/450",
+    "https://picsum.photos/id/234/800/450",
+    "https://picsum.photos/id/456/800/450",
+    "https://picsum.photos/id/789/800/450",
+    "https://picsum.photos/id/235/800/450",
+    "https://picsum.photos/id/236/800/450",
+    "https://picsum.photos/id/237/800/450"
+];
 
 // Spread photos vertically with a bit randomness (= spread evenly with +-5vh)
-for (let i = 0; i < photos.length; i++) {
-    photos[i].style.top = getRandomizedNumber(100 / photos.length * i, 10) + "vh";
+// for (let i = 0; i < photos.length; i++) {
+//     photos[i].style.top = randomizeNumber(100 / photos.length * i, 10) + "vh";
+// }
+
+
+const interval = setInterval(() => {
+    if (photos.length > 0) {
+        const newPhotoElement = createPhotoElement(photos.shift());
+        setPhotoVerticalPosition(newPhotoElement);
+        animatePhoto(newPhotoElement);
+    }
+}, 2000);
+
+// Create a new photo element
+function createPhotoElement (src) {
+    const newElement = document.createElement('img');
+    newElement.src = src;
+    newElement.className = "photo";
+    document.getElementById('container').appendChild(newElement);
+    return newElement;
 }
 
-// Add all photos
-while (photos.length > 0) {
-    addPhotoToTimeline(photos.shift());
-}
-
-// Add a new photo to the animation timeline
-function addPhotoToTimeline (photo) {
+// Create animation for photo
+function animatePhoto (photo) {
     anime({
         targets: photo,
         translateX: '100vw',
         easing: 'easeInOutSine',
-        duration: 10000 + ( ( Math.random() - 0.5 ) * 2000 ),
-        delay: getRandomizedNumber(1000, 1000)
+        duration: randomizeNumber(10000, 2000),
+        delay: randomizeNumber(1000, 1000)
     });
 }
 
-// Randomize an number with a maximum deviation
-function getRandomizedNumber(number, maxDeviation) {
-    return Math.floor ( number + ( ( Math.random() - 0.5 ) * maxDeviation ) );
+// Randomise photo vertically
+function setPhotoVerticalPosition (photo) {
+    photo.style.top = randomizeNumber(40, 50) + "vh";
+}
+
+// Randomize an number with a maximum deviation that is either subtracted or added
+function randomizeNumber(number, maxDeviation) {
+    return Math.floor ( number + ( ( Math.random() - 0.5 ) * 2 * maxDeviation ) );
 }
