c8ec40aec82fd1d8a76558fa2b2b589836aaa5d1
Ruud Verheijden <ruud.verheijden@gmail.com>
2021-03-19 22:01:58 +0100

    Make sure photo animation starts and ends off screen + add randomised rotate and scale transformations

--- src/view/slideshowView.ts
+++ src/view/slideshowView.ts
@@ -27,6 +27,7 @@ export default class slideshowView {
   private createPhotoElement(id: number, src: string): void {
     const newElement = document.createElement("img");
     newElement.src = src;
+    newElement.id = `photo-${id}`;
     newElement.className = "photo";
 
     // Store element reference for quick reference
@@ -57,7 +58,12 @@ export default class slideshowView {
         } else {
           const animation = anime({
             targets: photoReference?.element,
-            translateX: "100vw",
+            translateX: `${
+              this.container.offsetWidth +
+              photoReference.element.offsetWidth * 1.3
+            }px`, // Make sure photo ends out of the container, even if it's rotated
+            rotate: `${slideshowView.randomizeNumber(0, 10)}deg`,
+            scale: slideshowView.randomizeNumber(1.0, 0.3, false),
             easing: "easeInOutSine",
             duration: slideshowView.randomizeNumber(10000, 2000),
             delay: slideshowView.randomizeNumber(1000, 1000),
@@ -76,12 +82,18 @@ export default class slideshowView {
     const photoElement = this.photos.get(id)?.element || undefined;
 
     if (photoElement) {
-      photoElement.style.left = `${slideshowView.randomizeNumber(0, 5)}vw`;
+      photoElement.style.left = `-${photoElement.offsetWidth}px`;
       photoElement.style.top = `${slideshowView.randomizeNumber(40, 50)}vh`;
     }
   }
 
-  private static randomizeNumber(number: number, maxDeviation: number): number {
-    return Math.floor(number + (Math.random() - 0.5) * 2 * maxDeviation);
+  // Randomise number with a maximum deviation, defaults to convert to integer
+  private static randomizeNumber(
+    number: number,
+    maxDeviation: number,
+    toInteger: boolean = true
+  ): number {
+    const randomised = number + (Math.random() - 0.5) * 2 * maxDeviation;
+    return toInteger ? Math.floor(randomised) : randomised;
   }
 }
